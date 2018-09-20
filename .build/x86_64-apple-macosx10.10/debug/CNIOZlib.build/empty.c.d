25f6112f7b0c66cb257e868cbbdf6fcb50cd4b99
Khanh.Nt <khanh.nt@seabank.com.vn>
2018-09-20 14:27:01 +0700

    hello

---
+++ .build/x86_64-apple-macosx10.10/debug/CNIOZlib.build/empty.c.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Khanh/vapor/TILApp/.build/checkouts/swift-nio-zlib-support.git--1071467962839356487/module.modulemap \
+  /Users/Khanh/vapor/TILApp/.build/checkouts/swift-nio.git-3108475404973543938/Sources/CNIOZlib/empty.c
